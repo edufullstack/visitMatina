c033e19931aa25f88d7515a561c8344c048ad108
jose <joseerahe@gmail.com>
2023-10-23 14:40:37 -0700

    back arreglado4

--- README.md
+++ README.md
@@ -1,19 +1,7 @@
 # visitMatina
 
-Proyecto Final Henry - Web para Alianza Turística Marítima - Visit Matina
+Web para Alianza Turística Marítima - Visit Matina
 
-probando 123
-segundo intento
-=======
-
-tache a todos
-#testing Noe
-
-Test Leonel
-
-&&& test Leonel prueba
-
-&&& test Victor prueba
 
 # Paypal
 
--- api/src/app.js
+++ api/src/app.js
@@ -3,30 +3,38 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require('cors'); //Lisandro. Puse para pruebas. sacar
+const cors = require('cors');
 
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
+
+// Logging
 server.use(morgan('dev'));
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://visit-matina-tau.vercel.app'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// Body parsing
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
+
+// Cookies
+server.use(cookieParser());
+
+// Configura CORS
+const corsOptions = {
+  origin: 'https://visit-matina-tau.vercel.app', // Permite solo este origen
+  methods: 'GET, POST, OPTIONS, PUT, DELETE',
+  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  credentials: true,
+  maxAge: 86400, // Cache durante 1 día
+};
+
+server.use(cors(corsOptions));
+
+// Routes
 server.use('/api/', routes);
 
 // Error catching endware.
